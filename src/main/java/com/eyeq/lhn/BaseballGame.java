cdecf3ea2080c8bde9a1695c65a1d7321ca618ee
Hana Lee <i@leehana.co.kr>
2015-11-11 20:50:30 +0900

    LHN 기본 패키지 생성 및 게임 메인 클래스 생성.

---
+++ src/main/java/com/eyeq/lhn/BaseballGame.java
@@ -0,0 +1,12 @@
+package com.eyeq.lhn;
+
+/**
+ * @author Hana Lee
+ * @since 2015-11-11 20-47
+ */
+public class BaseballGame {
+
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/test/java/com/eyeq/lhn/BaseballGameTest.java
@@ -0,0 +1,14 @@
+package com.eyeq.lhn;
+
+import org.junit.Test;
+
+/**
+ * @author Hana Lee
+ * @since 2015-11-11 20-48
+ */
+public class BaseballGameTest {
+
+	@Test
+	public void test1() {
+	}
+}
