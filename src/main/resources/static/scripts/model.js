0fe5f156b8d198695910eef18bc42aabd5f7d9f6
Hana Lee <i@leehana.co.kr>
2016-03-21 21:16:54 +0900

    플레이어 및 게임룸, 게임룸 리스트를 좀 더 쉽게 관리 할 수 있도록 ValueObject 생성 및 vo 를 다룰 model 코드 생성

---
+++ src/main/resources/static/scripts/model.js
@@ -0,0 +1,129 @@
+/**
+ * @author Hana Lee
+ * @since 2016-03-21 19:50
+ */
+/*jslint
+ browser  : true,
+ continue : true,
+ devel    : true,
+ indent   : 2,
+ maxerr   : 100,
+ nomen    : true,
+ plusplus : true,
+ regexp   : true,
+ vars     : false,
+ white    : true,
+ todo     : true
+ */
+/*global $, app, webix, $$ */
+
+/**
+ * @namespace app
+ * @module model
+ *
+ * @description 플레이어, 게임룸, 게임룸 리스트를 저장하여 어플리케이션에서 활용 한다
+ */
+app.model = (function () {
+  'use strict';
+
+  var
+    /**
+     * @description 현재 로그인한 플레이어
+     *
+     * @type {Player|null}
+     * @private
+     */
+    player = null,
+    /**
+     * @description 입장한 게임룸
+     *
+     * @type {GameRoom|null}
+     * @private
+     */
+    gameRoom = null,
+    /**
+     * @description 생성된 게임룸 리스트
+     *
+     * @type {GameRoom[]}
+     * @private
+     */
+    gameRoomList = [],
+
+    /**
+     * @description 현재 로그인한 사용자의 정보를 설정 한다
+     *
+     * @param {Player|null} player_model
+     * @public
+     */
+    setPlayer = function (player_model) {
+      player = player_model;
+    },
+
+    /**
+     * @description 현재 로그인 한 사용자의 정보를 반환 한다
+     *
+     * @returns {Player|null}
+     * @public
+     */
+    getPlayer = function () {
+      return player;
+    },
+
+    /**
+     * @description 현재 입장한 게임룸의 정보를 설정 한다
+     *
+     * @param {GameRoom|null} gameRoom_model
+     * @public
+     */
+    setGameRoom = function (gameRoom_model) {
+      gameRoom = gameRoom_model;
+    },
+
+    /**
+     * @description 현재 입장한 게임룸의 정보를 반환 한다
+     *
+     * @returns {GameRoom|null}
+     * @public
+     */
+    getGameRoom = function () {
+      return gameRoom;
+    },
+
+    /**
+     * @description 현재 생성된 게임룸 리스트를 설정 한다
+     *
+     * @param {GameRoom[]|null} gameRoomList_model
+     * @public
+     */
+    setGameRoomList = function (gameRoomList_model) {
+      gameRoomList = gameRoomList_model;
+    },
+
+    /**
+     * @description 현재 설정된 게임룸 리스트를 반환 한다
+     *
+     * @returns {GameRoom[]|null}
+     * @public
+     */
+    getGameRoomList = function () {
+      return gameRoomList;
+    },
+
+    /**
+     * @description 새로운 게임룸을 게임룸 리스트에 추가 한다
+     * @param {GameRoom} newGameRoom
+     */
+    addGameRoom = function (newGameRoom) {
+      gameRoomList.push(newGameRoom);
+    };
+
+  return {
+    setPlayer : setPlayer,
+    getPlayer : getPlayer,
+    setGameRoom : setGameRoom,
+    getGameRoom : getGameRoom,
+    setGameRoomList : setGameRoomList,
+    getGameRoomList : getGameRoomList,
+    addGameRoom : addGameRoom
+  };
+}());
---
+++ src/main/resources/static/scripts/vo.js
@@ -0,0 +1,333 @@
+/**
+ * @author Hana Lee
+ * @since 2016-03-21 19:51
+ */
+/*jslint
+ browser  : true,
+ continue : true,
+ devel    : true,
+ indent   : 2,
+ maxerr   : 100,
+ nomen    : true,
+ plusplus : true,
+ regexp   : true,
+ vars     : false,
+ white    : true,
+ todo     : true
+ */
+/*global $, app, webix, $$ */
+
+app.vo = (function () {
+  'use strict';
+
+  var
+    /**
+     * @typedef {Object} Avatar
+     * @type {Avatar}
+     *
+     * @property {Number} id
+     * @property {String} imagePath
+     * @property {String} imageExt
+     * @property {String} mimeType
+     */
+    avatar = {
+      id : null,
+      imagePath : null,
+      imageExt : null,
+      mimeType : null
+    },
+    /**
+     * @typedef {Object} Level
+     * @type {Level}
+     *
+     * @property {Number} id
+     * @property {Number} value
+     */
+    level = {
+      id : null,
+      value : null
+    },
+    /**
+     * @typedef {Object} TotalScore
+     * @type {TotalScore}
+     *
+     * @property {Number} id
+     * @property {Number} value
+     */
+    totalScore = {
+      id : null,
+      value : null
+    },
+    /**
+     * @typedef {Object} TotalRank
+     * @type {TotalRank}
+     *
+     * @property {Number} id
+     * @property {Number} value
+     */
+    totalRank = {
+      id : null,
+      value : null
+    },
+    /**
+     * @typedef {Object} TotalGame
+     * @type {TotalGame}
+     *
+     * @property {Number} id
+     * @property {Number} count
+     */
+    totalGame = {
+      id : null,
+      count : null
+    },
+    /**
+     * @typedef {Object} Win
+     * @type {Win}
+     *
+     * @property {Number} id
+     * @property {Number} count
+     */
+    win = {
+      id : null,
+      count : null
+    },
+    /**
+     * @typedef {Object} Lose
+     * @type {Lose}
+     *
+     * @property {Number} id
+     * @property {Number} count
+     */
+    lose = {
+      id : null,
+      count : null
+    },
+    /**
+     * @typedef {Object} MatchRecord
+     * @type {MatchRecord}
+     *
+     * @property {Number} id
+     * @property {TotalGame} totalGame
+     * @property {Win} win
+     * @property {Lose} lose
+     */
+    matchRecord = {
+      id : null,
+      totalGame : totalGame,
+      win : win,
+      lose : lose
+    },
+    /**
+     * @typedef {Object} AttackerRoleCount
+     * @type {AttackerRoleCount}
+     *
+     * @property {Number} id
+     * @property {Number} value
+     */
+    attackerRoleCount = {
+      id : null,
+      value : null
+    },
+    /**
+     * @typedef {Object} DefenderRoleCount
+     * @type {DefenderRoleCount}
+     *
+     * @property {Number} id
+     * @property {Number} value
+     */
+    defenderRoleCount = {
+      id : null,
+      value : null
+    },
+    /**
+     * @typedef {Object} Settlement
+     * @type {Settlement}
+     *
+     * @property {Number} id
+     * @property {Boolean} solved
+     */
+    settlement = {
+      id : null,
+      solved : null
+    },
+    /**
+     * @typedef {Object} Strike
+     * @type {Strike}
+     *
+     * @property {Number} id
+     * @property {Number} value
+     */
+    strike = {
+      id : null,
+      value : null
+    },
+    /**
+     * @typedef {Object} Ball
+     * @type {Ball}
+     *
+     * @property {Number} id
+     * @property {Number} value
+     */
+    ball = {
+      id : null,
+      value : null
+    },
+    /**
+     * @typedef {Object} Result
+     * @type {Result}
+     *
+     * @property {Number} id
+     * @property {Settlement} settlement
+     * @property {Strike} strike
+     * @property {Ball} ball
+     */
+    result = {
+      id : null,
+      settlement : settlement,
+      strike : strike,
+      ball : ball
+    },
+    /**
+     * @typedef {Object} Rank
+     * @type {Rank}
+     *
+     * @property {Number} id
+     * @property {Number} value
+     */
+    rank = {
+      id : null,
+      value : null
+    },
+    /**
+     * @typedef {Object} Score
+     * @type {Score}
+     *
+     * @property {Number} id
+     * @property {Number} value
+     */
+    score = {
+      id : null,
+      value : null
+    },
+    /**
+     * @typedef {Object} Player
+     * @type {Player}
+     *
+     * @property {Number} id
+     * @property {String} email
+     * @property {String} nickname
+     * @property {Avatar} avatar
+     * @property {Level} level
+     * @property {TotalScore} totalScore
+     * @property {TotalRank} totalRank
+     * @property {MatchRecord} matchRecord
+     * @property {AttackerRoleCount} attackerRoleCount
+     * @property {DefenderRoleCount} defenderRoleCount
+     * @property {String} gameRole
+     * @property {String} status
+     * @property {Number} created
+     * @property {Number} updated
+     * @property {Number} deleted
+     * @property {Boolean} admin
+     * @property {String} enabled
+     * @property {Number} inputCount
+     * @property {Number} wrongCount
+     * @property {String} guessNumber
+     * @property {Result} result
+     * @property {Rank} rank
+     * @property {Score} score
+     * @property {Number} gameOverTime
+     */
+    player = {
+      id : null,
+      email : null,
+      nickname : null,
+      avatar : avatar,
+      level : level,
+      totalScore : totalScore,
+      totalRank : totalRank,
+      matchRecord : matchRecord,
+      attackerRoleCount : attackerRoleCount,
+      defenderRoleCount : defenderRoleCount,
+      gameRole : null,
+      status : null,
+      created : null,
+      updated : null,
+      deleted : null,
+      admin : null,
+      enabled : null,
+      inputCount : null,
+      wrongCount : null,
+      guessNumber : null,
+      result : result,
+      rank : rank,
+      score : score,
+      gameOverTime : null
+    },
+    /**
+     * @typedef {Object} Setting
+     * @type {Setting}
+     * 
+     * @property {Number} id
+     * @property {Number} limitWrongInputCount
+     * @property {Number} limitGuessInputCount
+     * @property {Number} generationNumberCount
+     */
+    setting = {
+      id : null,
+      limitWrongInputCount : null,
+      limitGuessInputCount : null,
+      generationNumberCount : null
+    },
+    /**
+     * @typedef {Object} GameNumber
+     * @type {GameNumber}
+     *
+     * @property {Number} id
+     * @property {Number} value
+     */
+    gameNumber = {
+      id : null,
+      value : null
+    },
+    /**
+     * @typedef {Object} GameRoom
+     * @type {GameRoom}
+     *
+     * @property {Number} id
+     * @property {String} name
+     * @property {Number} roomNumber
+     * @property {Number} limitPlayerCount
+     * @property {String} status
+     * @property {Player} owner
+     * @property {Player[]} players
+     * @property {Setting} setting
+     * @property {Number} gameCount
+     * @property {GameNumber} gameNumber
+     * @property {Number} created
+     * @property {Number} updated
+     * @property {Number} deleted
+     * @property {String} enabled
+     */
+    gameRoom = {
+      id : null,
+      name : null,
+      roomNumber : null,
+      limitPlayerCount : null,
+      status : null,
+      owner : player,
+      players : [],
+      setting : setting,
+      gameCount : null,
+      gameNumber : gameNumber,
+      created : null,
+      updated : null,
+      deleted : null,
+      enabled : null
+    };
+
+  return {
+    player : player,
+    gameRoom : gameRoom
+  };
+}());
